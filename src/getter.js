48fe642e104dbf47f6d8697faa4e568d35047391
Tim Routowicz <troutowicz@gmail.com>
2015-09-18 09:48:39 -0400

    Fix getter default options

--- src/getter.js
+++ src/getter.js
@@ -2,17 +2,13 @@ import request from 'request';
 
 import parse from './parser';
 
-export default function getter(args) {
-  const defaultOptions = {
+export default function getter(args = {}) {
+  const options = {
     url: 'http://predb.me',
     headers:  {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+      'User-Agent': args.userAgent ||
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
     },
-  };
-
-  const options = {
-    ...defaultOptions,
-    headers: { 'User-Agent': args.userAgent },
   };
 
   return {
